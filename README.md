015f85b6e286a59bffa300fa71cf2c9cbd1738a2
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-03-08 09:12:37 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vanna Ai Local
+
+- **Run services**:
+
+```sh
+docker compose exec -it ollama-llm /bin/sh
+```
+
+- **Pull LLM model**:
+
+```sh
+ollama pull mistral
+```
+
+- **Run LLM model**:
+
+```sh
+ollama run mistral
+```
+
+- [vanna.ai](https://vanna.ai/docs/sqlite-ollama-chromadb/)
---
+++ streamlit/src/main.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from vanna.vannadb.vannadb_vector import VannaDB_VectorStore
+from vanna.chromadb.chromadb_vector import ChromaDB_VectorStore
+from vanna.ollama import Ollama
+
+
+class MyVanna(ChromaDB_VectorStore, Ollama):
+    def __init__(self, config=None):
+        Ollama.__init__(self, config=config)
+        vs = ChromaDB_VectorStore.__init__(self, config=config)
+
+
+vn = MyVanna(
+    config={"model": "mistral", "ollama_host": "http://ollama-llm:11434"}
+)
+vn.connect_to_sqlite("https://vanna.ai/Chinook.sqlite")
+
+if not st.session_state.get("trained"):
+    df_ddl = vn.run_sql(
+        "SELECT type, sql FROM sqlite_master WHERE sql is not null"
+    )
+
+    for ddl in df_ddl["sql"].to_list():
+        vn.train(ddl=ddl)
+    st.session_state["trained"] = True
+
+
+my_question = st.session_state.get("my_question", default=None)
+if my_question is None:
+    my_question = st.text_input(
+        "Ask me a question that I can turn into SQL", key="my_question"
+    )
+else:
+    st.title(my_question)
+    sql = vn.generate_sql(my_question)
+    st.code(sql, language="sql")
+    df = vn.run_sql(sql)
+    st.dataframe(df, use_container_width=True)
+    fig = vn.get_plotly_figure(
+        plotly_code=vn.generate_plotly_code(
+            question=my_question, sql=sql, df=df
+        ),
+        df=df,
+    )
+    st.plotly_chart(fig, use_container_width=True)
+    st.button(
+        "Ask another question", on_click=lambda: st.session_state.clear()
+    )
